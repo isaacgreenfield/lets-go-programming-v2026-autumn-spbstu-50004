f5210c39483b5ab4b94026ef9efa241c7661da22
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2026-09-20 17:34:55 +0300

    [TASK-1] Attempted bug fix 3

--- ivanov.timur/task-1/cmd/service/main.go
+++ ivanov.timur/task-1/cmd/service/main.go
@@ -3,23 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
-	var operation string
+	var a, b int32
+	var op string
 
 	_, err := fmt.Scan(&a)
 	if err != nil {
 		fmt.Println("Invalid first operand")
+		return
 	}
 	_, err = fmt.Scan(&b)
 	if err != nil {
 		fmt.Println("Invalid second operand")
+		return
 	}
-	_, err = fmt.Scan(&operation)
+	_, err = fmt.Scan(&op)
 	if err != nil {
 		fmt.Println("Invalid operation")
+		return
 	}
 
-	switch operation {
+	switch op {
 	case "+":
 		fmt.Println(a + b)
 	case "-":
